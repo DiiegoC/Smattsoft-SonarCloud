af3c85c42b6ed84a404fc0674c0f5c0a0a7d213e
cruzmorningstar <79116446+DiiegoC@users.noreply.github.com>
2021-05-13 11:38:33 -0500

    Update ReglasSwift.swift

--- ReglasSwift.swift
+++ ReglasSwift.swift
@@ -7,11 +7,9 @@
 // MARK: REGLAS PARA CODIFICACIÓN IOS
 
 ////  Parameter - SINTAXIS
-///
 /// # Los archivos deberán de contener 400 líneas de código como máximo
 /// # Las clases deberán de contener 200 líneas de código como máximo, utilizar extensiones para lograr esta cantidad.
 /// # Las líneas de código deberán de ser menores a 120 caracteres (recomendación)
-
 
 ////  Parameter - SINTAXIS
 /// # Las variables deberán de estar en notación de camello
